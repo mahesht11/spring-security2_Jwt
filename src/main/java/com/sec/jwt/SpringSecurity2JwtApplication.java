91f493abc6aaf1eb2e173e587657bbe03ef89d37
mahesht11 <maheshgtjava@gmail.com>
2025-12-02 22:22:42 +0530

    commit

---
+++ src/main/java/com/sec/jwt/SpringSecurity2JwtApplication.java
@@ -0,0 +1,13 @@
+package com.sec.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity2JwtApplication.class, args);
+	}
+
+}
